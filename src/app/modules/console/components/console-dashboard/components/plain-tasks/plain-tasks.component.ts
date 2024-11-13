c7de08f76344954a97e1c5b0530203bbb01d1259
Tasheela Jayawickrama <tasheelajay1999@gmail.com>
2024-11-13 10:30:42 +0530

    plain task one without styling

--- src/app/modules/console/components/console-dashboard/components/plain-tasks/plain-tasks.component.ts
+++ src/app/modules/console/components/console-dashboard/components/plain-tasks/plain-tasks.component.ts
@@ -1,6 +1,13 @@
 import {Component, OnInit} from '@angular/core';
-import {CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem} from "@angular/cdk/drag-drop";
-import {NgForOf, NgIf} from "@angular/common";
+import {
+  CdkDrag,
+  CdkDragDrop,
+  CdkDropList,
+  CdkDropListGroup,
+  moveItemInArray,
+  transferArrayItem
+} from "@angular/cdk/drag-drop";
+import {NgClass, NgForOf, NgIf} from "@angular/common";
 import {MatButton} from "@angular/material/button";
 import {YOUTUBE_PLAYER_CONFIG, YouTubePlayer} from "@angular/youtube-player";
 import {MatFormField, MatInput, MatLabel} from "@angular/material/input";
@@ -27,7 +34,9 @@ import {
     MatAccordion,
     MatExpansionPanel,
     MatExpansionPanelTitle,
-    MatExpansionPanelHeader
+    MatExpansionPanelHeader,
+    NgClass,
+    CdkDropListGroup
   ],
   providers: [{
     provide: YOUTUBE_PLAYER_CONFIG,
@@ -40,22 +49,16 @@ import {
 })
 export class PlainTasksComponent{
   videoId: string = '';
+  items = [''];
 
-  extractVideoId(event: Event): void {
-    const inputElement = event.target as HTMLInputElement;
-    const url = inputElement?.value || ''; // Ensure value is defined
-    const videoIdMatch = url.match(/(?:youtube\.com\/.*v=|youtu\.be\/)([^&]+)/);
-    this.videoId = videoIdMatch ? videoIdMatch[1] : '';
-  }
+  basket = [''];
 
+  searchItem = '';
+  itemFound = false;
 
-
-
-  todo = ['1', '2',  '3'];
-  done = ['1', '2',  '3'];
-
-  //done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
-
+  counter = 0;
+  isStarted = false;
+  isAnswerCorrect = false;
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
@@ -68,7 +71,100 @@ export class PlainTasksComponent{
         event.currentIndex,
       );
     }
+    this.checkAnswer()
   }
 
+  checkAnswer(){
+    for (let i = 0; i < this.basket.length; i++) {
+      if (this.basket[i] === this.searchItem) {
+        this.itemFound = true;
+        this.setAlerts( this.itemFound)
+        break;
+      }
+      else{
+        this.setAlerts( this.itemFound)
+      }
+    }
+  }
+
+  setAlerts(answer:boolean){
+    if (answer) {
+      console.log(`${this.searchItem} is available in the items array.`);
+      this.isAnswerCorrect = true;
+
+      setTimeout(() => {
+        this.moveToNext()
+      }, 1000);
+
+    } else {
+      console.log(`${this.searchItem} is not available in the items array.`);
+    }
+
+  }
+
+
+  dataList:any = [
+    {
+      itemlist:['Carrots', 'Tomatoes', 'Onions', 'Apples', 'Avocados','Bananas'],
+      searchItem: 'Onions'
+    },
+    {
+      itemlist:['Red', 'Green', 'Blue', 'Yellow', 'White','Black'],
+      searchItem: 'Blue'
+    },
+    {
+      itemlist:['1', '10', '20', '30', '50','60'],
+      searchItem: '50'
+    },
+  ]
+
+  start(){
+    this.items = this.dataList[0].itemlist;
+    this.searchItem= this.dataList[0].searchItem;
+    this.isStarted = true;
+  }
+
+  moveToNext(){
+    this.counter +=1;
+    this.reset();
+
+    this.items = this.dataList[this.counter].itemlist;
+    this.searchItem= this.dataList[this.counter].searchItem;
+  }
+
+  reset(){
+    this.isAnswerCorrect = false;
+    this.itemFound = false;
+    this.items = [];
+    this.searchItem = '';
+    this.basket = [];
+  }
+  reStartGame(){
+    this.reset();
+    this.counter = 0;
+    this.isStarted= false;
+
+    this.dataList = [
+      {
+        itemlist:['Carrots', 'Tomatoes', 'Onions', 'Apples', 'Avocados','Bananas'],
+        searchItem: 'Onions'
+      },
+      {
+        itemlist:['Red', 'Green', 'Blue', 'Yellow', 'White','Black'],
+        searchItem: 'Blue'
+      },
+      {
+        itemlist:['1', '10', '20', '30', '50','60'],
+        searchItem: '50'
+      },
+    ]
+  }
+
+  extractVideoId(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    const url = inputElement?.value || ''; // Ensure value is defined
+    const videoIdMatch = url.match(/(?:youtube\.com\/.*v=|youtu\.be\/)([^&]+)/);
+    this.videoId = videoIdMatch ? videoIdMatch[1] : '';
+  }
   protected readonly HTMLInputElement = HTMLInputElement;
 }
