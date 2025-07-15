ec6adf49d3b00a87da0e1c176f474382ee82076b
Tasheela Jayawickrama <tasheelajay1999@gmail.com>
2025-07-15 10:55:05 +0530

    new changes

--- src/app/components/student-name-popup/student-name-popup.component.ts
+++ src/app/components/student-name-popup/student-name-popup.component.ts
@@ -1,8 +1,17 @@
 import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {AttentionService} from "../../services/attention.service";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+
+interface AttentionData {
+  student_id: string;
+  interval_data: {
+    interval_start: number;
+    overall_attention: number;
+  }[];
+}
+
 
 @Component({
   selector: 'app-student-name-popup',
@@ -22,21 +31,49 @@ export class StudentNamePopupComponent {
   private attentionService = inject(AttentionService);
 
   popupForm = new FormGroup({
-    studentName:new FormControl(),
-    roundNumber:new FormControl(),
+    studentName: new FormControl(),
+    roundNumber: new FormControl(),
   })
-
   submitDetails(): void {
     this.attentionService.trackAttention(
       this.popupForm.get('studentName')?.value,
       this.popupForm.get('roundNumber')?.value,
     ).subscribe(response => {
       console.log(response);
-    })
+
+      const studentName = this.popupForm.get('studentName')?.value;
+      let intervalIndex = 0;
+
+      const intervalId = setInterval(() => {
+        this.attentionService.getStudentAttentionLevel(studentName).subscribe((response: any) => {
+          const overallData = response?.data.overall ?? [];
+
+          if (overallData.length > 0) {
+            const currentOverallAttention = overallData[intervalIndex];
+            console.log(`Overall Attention: ${currentOverallAttention}`);
+
+            if (currentOverallAttention < 65) {
+              console.log("you lost your attention");
+            }
+
+            intervalIndex++;
+
+            if (intervalIndex >= overallData.length) {
+              clearInterval(intervalId);
+            }
+          } else {
+            console.error("No overall data found or array is empty.");
+            clearInterval(intervalId);
+          }
+        });
+      }, 10000);
+    });
   }
 
+
+
   cancel(): void {
-   this.dialogRef.close();
+    this.dialogRef.close();
   }
 
   onKeyPress(event: KeyboardEvent): void {
